39c9fcf6e21678e01f2f213bbff1cf6e6e53f3b2
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-11-29 13:31:35 +0100

    first push

---
+++ QUIZZIZ/app/src/main/java/com/example/quizziz/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.quizziz
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ediTextName: EditText = findViewById(R.id.edit_text_name)
+        val btnStart: Button = findViewById(R.id.button_id)
+
+        btnStart.setOnClickListener {
+            Log.i("mylog", "Ok")
+            if (ediTextName.text.isEmpty()) {
+                Toast.makeText(this, "proszę podać imię", Toast.LENGTH_LONG)
+                Log.i("mylog", "podaj imię")
+            } else {
+                Log.i("mylog", "Ok")
+            }
+        }
+    }
+}
---
+++ QUIZZIZ/app/src/main/java/com/example/quizziz/QuizzQuestionsAcivity.kt
@@ -0,0 +1,11 @@
+package com.example.quizziz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizzQuestionsAcivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quizz_questions_acivity)
+    }
+}
