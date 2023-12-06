206d267451221fc09690da3b3820496fbe8c1e18
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-12-06 13:31:59 +0100

    second upload

--- QUIZZIZ/app/src/main/java/com/example/quizziz/QuizzQuestionsAcivity.kt
+++ QUIZZIZ/app/src/main/java/com/example/quizziz/QuizzQuestionsAcivity.kt
@@ -1,15 +1,33 @@
 package com.example.quizziz
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 
-class QuizzQuestionsAcivity : AppCompatActivity() {
+class QuizzQuestionsAcivity : AppCompatActivity() , View.OnClickListener {
+    private var progessBar: ProgressBar? = null
+    private var imageView: Image? = null
+    private var textViewProgressBar: TextView? = null
+    private var textViewQuestion: TextView? = null
+    private var textViewOptionOne: TextView? = null
+    private var textViewOptionTwo: TextView? = null
+    private var textViewOptionThree: TextView? = null
+    private var textViewOptionFour: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz_questions_acivity)
 
         val questionList = Constans.getQuestions()
-        Log.i("my_info", "questionList = ${questionList.size}")
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
