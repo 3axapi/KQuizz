def04dafc9d6f27532c06f85df8f4b11723c6f8e
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-12-11 15:21:36 +0100

    3-4 commit

--- QUIZZIZ/app/src/main/java/com/example/quizziz/QuizzQuestionsAcivity.kt
+++ QUIZZIZ/app/src/main/java/com/example/quizziz/QuizzQuestionsAcivity.kt
@@ -1,5 +1,7 @@
 package com.example.quizziz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,8 +11,11 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.example.quiz.Question
 
 class QuizzQuestionsAcivity : AppCompatActivity() , View.OnClickListener {
+    private var questionsList: ArrayList<Question>? = null
+    private var currentIndex: Int = 1
     private var progessBar: ProgressBar? = null
     private var imageView: ImageView? = null
     private var buttonSubmit: Button? = null
@@ -20,7 +25,6 @@ class QuizzQuestionsAcivity : AppCompatActivity() , View.OnClickListener {
     private var textViewOptionTwo: TextView? = null
     private var textViewOptionThree: TextView? = null
     private var textViewOptionFour: TextView? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +44,40 @@ class QuizzQuestionsAcivity : AppCompatActivity() , View.OnClickListener {
         textViewOptionFour = findViewById(R.id.textview_option_four)
 
         buttonSubmit = findViewById(R.id.buttonview_button)
+    }
+
+    private fun setQuestion () {
+        val question: Question = questionsList!![currentIndex - 1]
+        textViewQuestion?.text = question.question
+        imageView?.setImageResource(question.image)
+        progessBar?.progress = currentIndex
+        textViewProgressBar?.text = "${currentIndex}/${progessBar?.max}"
+
+        textViewOptionOne?.text = question.optionOne
+        textViewOptionTwo?.text = question.optionTwo
+        textViewOptionThree?.text = question.optionThree
+        textViewOptionFour?.text = question.optionFour
+
+        if (currentIndex == questionsList!!.size) buttonSubmit?.text = "KONIEC"
+        else buttonSubmit?.text = "NASTĘPNE PYTANIE"
+    }
+
+    private fun defaultOptionsView () {
+        val options = ArrayList<TextView?>()
+        // let {} ===> if (!null) { code... }
+        textViewOptionOne.let {
+            options.add(0, it) }
+        textViewOptionTwo.let {
+            options.add(0, it) }
+        textViewOptionThree.let {
+            options.add(0, it) }
+        textViewOptionFour.let {
+            options.add(0, it) }
+
+        for (option in options) {
+            option?.setTextColor(Color.parseColor("#7a8089"))
+            option?.typeface = Typeface.DEFAULT
+        }
     }
 
     override fun onClick(v: View?) {
